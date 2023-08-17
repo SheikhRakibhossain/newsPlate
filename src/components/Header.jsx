639c5d9ef1ba68847c329c12c5782b2a76918912
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-17 11:47:45 +0600

    text slider or text marquee added with the rig right to left looping features

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,17 @@
 import moment from 'moment';
+import Marquee from "react-fast-marquee";
+
 
 const Header = () => {
     return (
         <>
             <div>
                 {moment().format('MMMM Do YYYY, h:mm:ss a')}
+            </div>
+            <div>
+                <Marquee>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum officiis magni corrupti id exercitationem distinctio quaerat recusandae autem quae? Fuga laudantium magni veritatis nam. Recusandae nisi cupiditate voluptatum accusamus rem! I can be a React component, multiple React components, or just some text.
+                </Marquee>
             </div>
         </>
     );
