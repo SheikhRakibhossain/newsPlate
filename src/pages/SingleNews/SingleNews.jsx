e6f9304fcecde997b36bab9151ccb54b958ec599
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-18 11:43:05 +0600

    left and right sidebar creation

--- src/pages/SingleNews/SingleNews.jsx
+++ src/pages/SingleNews/SingleNews.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 const SingleNews = () => {
     return (
-        <div>
-            <h2>Hello news layout</h2>
-        </div>
+        <div className="container mx-auto px-4 grid grid-cols-12">
+
+            
+
+                {/* main outlet here */}
+                <div className=" col-span-6"> <Outlet /> </div>
+
+                {/* Right side bar here */}
+                <div className="col-span-3"> <h2 className="text-xl font-bold">Left nav bar</h2></div>
+
+            </div>
     );
 };
 
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -45,7 +45,7 @@ const router = createBrowserRouter([
       children:[
 
        {
-        path:'/:id',
+        path:':id',
         element:<NewsDetails/>
        }
       ]
